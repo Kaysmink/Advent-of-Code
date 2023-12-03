18515f3a6c1c905abe4d23e4f21d392953792bac
kaysmink <kaysmink@live.nl>
2023-12-03 11:39:50 +0100

    Added Day 3

--- src/2023/Day 02. Cube Conundrum.py
+++ src/2023/Day 02. Cube Conundrum.py
@@ -9,9 +9,11 @@ max_dict = {"red": 12, "green": 13, "blue": 14}
 games_colours = [re.findall(r'[a-zA-Z]+', line)[1:] for line in Input]
 games_values = [re.findall(r'\d+', line)[1:] for line in Input]
 
-possible = [[int(games_values[y][x]) <= max_dict[colour]
-             for x, colour in enumerate(game)] for y, game in enumerate(games_colours)]
+possible = [[int(games_values[y][x]) <= max_dict[colour] for x, colour in enumerate(game)]
+            for y, game in enumerate(games_colours)]
 part1 = sum([game_num+1 for game_num, game in enumerate(possible) if all(game)])
 
-part2 = sum([np.prod(pd.DataFrame(list(map(int, games_values[y])), games_colours[y]).reset_index(
-).groupby("index").max()[0].tolist()) for y, game in enumerate(games_colours)])
+part2 = sum([np.prod(pd.DataFrame(list(map(int, games_values[y])), games_colours[y]).reset_index().groupby(
+    "index").max()[0].tolist()) for y, game in enumerate(games_colours)])
+
+print(part1, part2)
---
+++ src/2023/Day 03. Gear Ratios.py
@@ -0,0 +1,47 @@
+import regex as re
+import numpy as np
+
+
+def findNeighbors(x, y):
+    return [[xn, yn] for xn in range(x-1, x+2) for yn in range(y-1, y+2) if 0 <= xn < len(Input[0]) and 0 <= yn < len(Input) and (abs(x-xn) + abs(y-yn) > 0)]
+
+
+# Part 1
+Input = open("data/2023/dag 03. input.txt", "r").read().split("\n")[0:-1]
+
+number_coords = [[[match.start(), match.end()] for match in re.finditer(r'\d+', line)] for line in Input]
+full_number_coords = [[[[y, x] for x in range(xmin, xmax)]
+                       for xmin, xmax in number_coords[y]] for y in range(0, len(Input))]
+numbers = [[int(match.group()) for match in re.finditer(r'\d+', line)] for line in Input]
+Input = [list(line) for line in Input]
+symbol_coords = [[xn, yn] for xn in range(0, len(Input[1])) for yn in range(
+    0, len(Input)) if not Input[yn][xn].isdigit() and Input[yn][xn] != "."]
+
+
+part1 = 0
+for y, line in enumerate(numbers):
+    for x, number in enumerate(line):
+        coords = full_number_coords[y][x]
+        neighbors = [findNeighbors(xn, yn) for yn, xn in coords]
+        neighbors = [coord for number in neighbors for coord in number]
+
+        if any([neighbor in symbol_coords for neighbor in neighbors]):
+            part1 = part1 + number
+
+
+# Part2
+gear_coords = [[yn, xn] for xn in range(0, len(Input[1])) for yn in range(0, len(Input)) if Input[yn][xn] == "*"]
+
+part2 = 0
+for xg, yg in gear_coords:
+    num = []
+    neighbors = findNeighbors(xg, yg)
+    for y, line in enumerate(numbers):
+        for x, number in enumerate(line):
+            coords = full_number_coords[y][x]
+            if any([coord in neighbors for coord in coords]):
+                num.append(number)
+    if len(num) == 2:
+        part2 = part2 + np.prod(num)
+
+print(part1, part2)
