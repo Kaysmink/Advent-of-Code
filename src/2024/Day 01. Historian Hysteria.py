3845bc6f9a5de5fb4d2997d742ddaea5dd538c28
kaysmink <kaysmink@live.nl>
2024-12-01 14:59:27 +0100

    Added 2024 Day 1

---
+++ src/2024/Day 01. Historian Hysteria.py
@@ -0,0 +1,8 @@
+Input = open("data/2024/dag 01. input.txt", "r").read().split("\n")[0:-1]
+Input = [list(map(int, line.split())) for line in Input]
+
+l1 = sorted([value1 for value1, value2 in Input])
+l2 = sorted([value2 for value1, value2 in Input])
+
+part1 = sum([abs(l1[x] - l2[x]) for x in range(len(l1))])
+part2 = sum([value * l2.count(value) for value in l1])
